9b3b638e5148691190a994728da55c71964e64a7
Navijack <johnny_nike@live.com.pt>
2024-06-17 17:38:45 +0200

    add s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
         </li>
         <li>
           <Link to='contact' smooth={true} duration={500}>
-            Contact
+            Contacts
           </Link>
         </li>
       </ul>
