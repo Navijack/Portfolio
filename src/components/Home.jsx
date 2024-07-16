bc2b9880b406060d1cd09e2f781d487b666b60d8
Johnny Costa <Johnny_nike@live.com.pt>
2024-07-16 17:25:57 +0200

    incr

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
             strings={['.Developer', '.Designer', '.Enthusiast']} typeSpeed={150} backSpeed={140} loop />
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a frontend developer specializing in building exceptional digital experiences. Currently, I’m focused on creating responsive and
+          I’m a frontend developer specializing in building incredible digital experiences. Currently, I’m focused on creating responsive and
           interactive web interfaces.
         </p>
       </div>
